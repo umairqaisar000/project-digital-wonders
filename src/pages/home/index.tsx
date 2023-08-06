ebe4a90d91ee29f1ab2e103d1bd78e06f6e8fbfd
umairqaisar <umairqaisar000@gmail.com>
2023-08-06 20:14:41 +0500

    feat: added hero section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,8 @@
-import Navbar from "src/components/Navbar";
+import Image from "next/image";
+import HeroSectionImage from "src/assets/images/hero-section-image.svg";
 import { useRouter } from "next/router";
+import Navbar from "src/components/Navbar";
+
 import styles from "./Home.module.scss";
 
 export default function Home() {
@@ -8,6 +11,28 @@ export default function Home() {
   return (
     <div className={styles["container"]}>
       <Navbar pathname={router.pathname} />
+      <div className={styles["hero-section-container"]}>
+        <div>
+          <h1>Need assistance with your assignments?</h1>
+          <p>
+            Struggling to manage your assignments? We can step in and ensure
+            your success.
+          </p>
+          <div className={styles["subject-container"]}>
+            <div className={styles["subject-heading"]}>Your Subject</div>
+            <div className={styles["subject-bottom-container"]}>
+              <h5>dropdown</h5>
+              <div className={styles["subject-continue-button"]}>continue</div>
+            </div>
+          </div>
+        </div>
+
+        <Image
+          className={styles["organization-logo"]}
+          src={HeroSectionImage}
+          alt="logo"
+        />
+      </div>
     </div>
   );
 }
