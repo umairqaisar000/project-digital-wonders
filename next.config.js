4fa60f9f5798d82516936e68254783f7c75a6b08
M Umair Qaisar <70581144+umairqaisar000@users.noreply.github.com>
2023-08-06 21:32:39 +0500

    Revert "Feat/homepage hero section"

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-    });
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
